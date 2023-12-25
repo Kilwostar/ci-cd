cdcc0f34510e5b7696c1bc67b72742ed8ec13202
Kilwo <sashakabach@gmail.com>
2023-12-25 17:56:47 +0200

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    env: {
-      node: true,
-      commonjs: true,
-      es2021: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    rules: {
-      // Add your project-specific rules here
-    },
-  };
-  
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,  // Add this line to specify the Jest environment
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // Add your project-specific rules here
+  },
+};
